03fd2f041358237ceb039232e6f770da67c78d77
Sam Larsen <slarsen@meta.com>
2025-01-24 07:48:22 -0800

    [BE] Remove test_ops.py from torch/testing/_internal/dynamo_test_failures.py
    ghstack-source-id: 668de5bb178bd997bc5aebba9cbdd44f811f8b37
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/145307

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -3133,7 +3133,6 @@ class TestCase(expecttest.TestCase):
         compiled = TEST_WITH_TORCHDYNAMO or TEST_WITH_AOT_EAGER or TEST_WITH_TORCHINDUCTOR
         # Is the class strict and compiling?
         strict_default = False
-        should_reset_dynamo = False
         if compiled:
             try:
                 path = inspect.getfile(type(test_cls))
@@ -3144,9 +3143,6 @@ class TestCase(expecttest.TestCase):
                     if TEST_WITH_TORCHINDUCTOR:
                         from .dynamo_test_failures import FIXME_inductor_non_strict
                         strict_default = filename not in FIXME_inductor_non_strict
-
-                        from .dynamo_test_failures import FIXME_inductor_dont_reset_dynamo
-                        should_reset_dynamo = filename not in FIXME_inductor_dont_reset_dynamo
                     else:
                         strict_default = True
             # inspect.getfile can fail with these
@@ -3167,8 +3163,7 @@ class TestCase(expecttest.TestCase):
                 strict_mode = strict_default
         nopython = getattr(test_cls, "dynamo_strict_nopython", False) and compiled
 
-        if strict_mode or should_reset_dynamo:
-            torch._dynamo.reset()
+        torch._dynamo.reset()
 
         torch.compiler.set_stance("default")
 
@@ -3242,8 +3237,7 @@ class TestCase(expecttest.TestCase):
 
             super_run(result=result)
 
-        if strict_mode or should_reset_dynamo:
-            torch._dynamo.reset()
+        torch._dynamo.reset()
 
         # Early terminate test if necessary.  If using pytest, use the -x flag instead
         if using_unittest and self._should_stop_test_suite():
